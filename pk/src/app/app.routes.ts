f7f803f762de34c288b8affee6eedb3f6a34b1d6
Md. Shimul Pramanik <shimulpk61100@gmail.com>
2026-05-05 00:32:19 +0600

    ADD

--- pk/src/app/app.routes.ts
+++ pk/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './component/home/home';
+import { ContactUs } from './component/contact-us/contact-us';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'contact', component: ContactUs}
+];
